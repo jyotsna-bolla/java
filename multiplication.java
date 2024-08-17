30b565a68c151b2bd93d18fc38f9e2cff9ecc96a
jyotsna-bolla <151710494+jyotsna-bolla@users.noreply.github.com>
2024-08-17 12:10:23 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Multiplication
+{
+public static void main(String args[])
+{
+int a=87;
+int b=10;
+float c=a*b;
+System.out.println("Multiplication of "+a+" and "+b+" is "+c);
+}
+}
