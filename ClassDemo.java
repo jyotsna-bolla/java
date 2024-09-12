8b6396e55e1aae67f40d5edcde82c09659261de8
jyotsna-bolla <151710494+jyotsna-bolla@users.noreply.github.com>
2024-09-12 12:38:28 +0530

    Create ClassDemo.java

---
+++ ClassDemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ class ClassDemo
+{
+	static String rollno;
+	static double avg;
+	public static void main(String args[])
+	{
+		rollno="23K61A05J4";
+		avg=94.6;
+		System.out.println("Roll no is "+rollno+"\nAverage is "+avg);
+	}
+}
+
