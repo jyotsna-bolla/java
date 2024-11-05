35fa9c5cdbad1fcff313844f030c69fb51102ca0
jyotsna-bolla <151710494+jyotsna-bolla@users.noreply.github.com>
2024-11-05 14:03:08 +0530

    Create WhoAmI.java

---
+++ WhoAmI.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class A
+{
+	void callMe()
+	{
+		System.out.println("I am from A");
+	}
+}
+class B extends A
+{
+	void callMe()
+	{
+		System.out.println("I am from B");
+	}
+}
+class WhoAmI
+{
+	public static void main(String args[])
+	{
+		A a1 = new A();
+		A a2 = new B();
+		B b = new B();
+		a1.callMe();
+		a2.callMe();
+		b.callMe();
+	}
+}
